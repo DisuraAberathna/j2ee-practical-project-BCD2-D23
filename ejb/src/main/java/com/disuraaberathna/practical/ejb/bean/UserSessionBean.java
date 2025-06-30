9657924ab97903f18b8125886959448d0a0eb8b4
Disura Aberathna <disura2005@gmail.com>
2025-06-30 11:30:53 +0530

    Update UserSessionBean.java

--- ejb/src/main/java/com/disuraaberathna/practical/ejb/bean/UserSessionBean.java
+++ ejb/src/main/java/com/disuraaberathna/practical/ejb/bean/UserSessionBean.java
@@ -1,7 +1,38 @@
 package com.disuraaberathna.practical.ejb.bean;
 
+import com.disuraaberathna.practical.core.model.User;
+import com.disuraaberathna.practical.ejb.remote.UserService;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Stateless
-public class UserSessionBean {
+public class UserSessionBean implements UserService {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public User getUser(long id) {
+        return em.find(User.class, id);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return em.createNamedQuery("User.findByEmail", User.class).setParameter("email", email).getSingleResult();
+    }
+
+    @Override
+    public void addUser(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        em.merge(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        em.remove(user);
+    }
 }
