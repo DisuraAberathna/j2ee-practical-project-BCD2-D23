866fb8a37d846705b57abed86943dd6f39d3597f
Disura Aberathna <disura2005@gmail.com>
2025-06-30 17:11:36 +0530

    Update Login.java

--- web/src/main/java/com/disuraaberathna/practical/web/action/Login.java
+++ web/src/main/java/com/disuraaberathna/practical/web/action/Login.java
@@ -1,5 +1,10 @@
 package com.disuraaberathna.practical.web.action;
 
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.AuthenticationStatus;
+import jakarta.security.enterprise.SecurityContext;
+import jakarta.security.enterprise.authentication.mechanism.http.AuthenticationParameters;
+import jakarta.security.enterprise.credential.UsernamePasswordCredential;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,8 +15,21 @@ import java.io.IOException;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
+    @Inject
+    private SecurityContext securityContext;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
 
+        AuthenticationParameters params = AuthenticationParameters.withParams().credential(new UsernamePasswordCredential(email, password));
+
+        AuthenticationStatus status = securityContext.authenticate(req, resp, params);
+        if (status == AuthenticationStatus.SUCCESS) {
+            resp.sendRedirect(req.getContextPath() + "/index.jsp");
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+        }
     }
 }
