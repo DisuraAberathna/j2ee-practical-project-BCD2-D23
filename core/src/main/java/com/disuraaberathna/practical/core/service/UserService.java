d4c791018433f546b91a12deae9dcb1610f8675b
Disura Aberathna <disura2005@gmail.com>
2025-06-30 15:44:51 +0530

    Create UserService.java

---
+++ core/src/main/java/com/disuraaberathna/practical/core/service/UserService.java
@@ -0,0 +1,13 @@
+package com.disuraaberathna.practical.core.service;
+
+import com.disuraaberathna.practical.core.model.User;
+import jakarta.ejb.Remote;
+
+@Remote
+public interface UserService {
+    User getUser(long id);
+    User getUserByEmail(String email);
+    void addUser(User user);
+    void updateUser(User user);
+    void deleteUser(User user);
+}
